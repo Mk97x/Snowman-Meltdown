7a109ba74984b9d23aaf18ff53c01643910f3494
martin koch <martinkoch71@yahoo.de>
2025-07-02 12:10:19 +0000

    split file into snowman.py, game_logic.py and ascii_art.py

--- snowman.py
+++ snowman.py
@@ -1,41 +1,27 @@
 import random
+import ascii_art as art
 
-# Snowman ASCII Art stages
-STAGES = [
-     # Stage 0: Full snowman
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     ( : ) 
-     """,
-     # Stage 1: Bottom part starts melting
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     """,
-     # Stage 2: Only the head remains
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     """,
-     # Stage 3: Snowman completely melted
-     """
-      ___  
-     /___\\ 
-     """
- ]
 
-# List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
+STAGES = art.STAGES
 
 def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    """Displays the current game state"""
+    print(STAGES[mistakes])
+    
+    display_word = ""
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    
+    print("Word:", display_word)
+    print()
 
 def play_game():
     secret_word = get_random_word()
@@ -43,12 +29,9 @@ def play_game():
     mistakes = 0
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
+    
 
-    # TODO: Build your game loop here.
-    # For now, simply prompt the user once:
-    print(STAGES[mistakes])
-    underlines = ["_"] * len(secret_word)
-    print(" ".join(underlines))
+    display_game_state(mistakes, secret_word, guessed_letters)
     
     while mistakes < 3:
         while True:
@@ -58,32 +41,37 @@ def play_game():
                 continue
             elif len(guess.strip()) == 1:
                 break
+        
+  
         if guess not in guessed_letters:
             guessed_letters.append(guess)
             print("You guessed:", guess)
         else: 
+            mistakes +=1
             print("You already guessed that letter - focus")
+            continue  
+
 
         if guess in secret_word:
-            index = secret_word.find(guess)
             print("That letter is correct")
-            print(STAGES[mistakes])
-            underlines[index] = guess
-            print(" ".join(underlines))
-            
         else:
-            print("That letter is not appear to be in the word")
+            print("That letter does not appear to be in the word")
             mistakes += 1
-            print(STAGES[mistakes])
-            print(" ".join(underlines))
+        
 
-        if "".join(underlines) != secret_word:
-            continue
-        else:
+        display_game_state(mistakes, secret_word, guessed_letters)
+        
+
+        if all(letter in guessed_letters for letter in secret_word):
             print(f"You correctly guessed the word {secret_word}")
             break
-            
         
-    
+
+        if mistakes >= 3:
+            print("Game Over! The snowman melted completely!")
+            print(f"The word was: {secret_word}")
+            False
+        
+
 if __name__ == "__main__":
     play_game()
