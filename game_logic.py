e5a274e9fc1c930c391dee72e4daf67caf2db56b
martin koch <martinkoch71@yahoo.de>
2025-07-06 11:35:08 +0000

    added comments, replay option

--- game_logic.py
+++ game_logic.py
@@ -4,24 +4,38 @@ import ascii_art as art
 
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 STAGES = art.STAGES
+ALPHABET = "abcdefghijklmnopqrstuvwxyz" # english alphabet to check input is in fact a letter
 
 def get_random_word():
     """Selects a random word from the list."""
-    return WORDS[random.randint(0, len(WORDS) - 1)]
+    return WORDS[random.randint(0, len(WORDS) - 1)] # choose random word from WQRDS
 
 def display_game_state(mistakes, secret_word, guessed_letters):
     """Displays the current game state"""
     print(STAGES[mistakes])
     
-    display_word = ""
+    display_word = "" # empty str to build the underlines-gui
     for letter in secret_word:
         if letter in guessed_letters:
-            display_word += letter + " "
+            display_word += letter + " " # if letter in word, add letter and space
         else:
-            display_word += "_ "
+            display_word += "_ " # else add underline+space
     
     print("Word:", display_word)
     print()
+
+def ask_to_replay():
+    play_again = input("Do you wanna play again? (y/n) ").lower().strip()
+    while True:
+        try:
+            if play_again == "y" or play_again == "yes":
+                play_game()
+                return None
+            else:
+                break
+        except TypeError:
+            print("I need y or n as input.")
+
 
 def play_game():
     secret_word = get_random_word()
@@ -30,16 +44,15 @@ def play_game():
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
     
-
     display_game_state(mistakes, secret_word, guessed_letters)
     
-    while mistakes < 3:
-        while True:
-            guess = input("Guess a letter: ").lower()
-            if len(guess.strip()) > 1:
+    while mistakes < 3: # 3 lifes
+        while True: # try input until it is valid
+            guess = input("Guess a letter: ").lower().strip() # strip for potential whitespaces
+            if len(guess) > 1 or guess not in ALPHABET:
                 print("Only single letter inputs are allowed")
                 continue
-            elif len(guess.strip()) == 1:
+            elif len(guess) == 1:
                 break
         
   
@@ -49,6 +62,7 @@ def play_game():
         else: 
             mistakes +=1
             print("You already guessed that letter - focus")
+            print(STAGES[mistakes])
             continue  
 
 
@@ -62,15 +76,18 @@ def play_game():
         display_game_state(mistakes, secret_word, guessed_letters)
         
 
-        if all(letter in guessed_letters for letter in secret_word):
+        if all(letter in guessed_letters for letter in secret_word): # check if all letters from secret word are guessed
             print(f"You correctly guessed the word {secret_word}")
             break
         
 
-        if mistakes >= 3:
+        if mistakes >= 3: # losing screen after 3 wrong tries
             print("Game Over! The snowman melted completely!")
             print(f"The word was: {secret_word}")
             False
+
+    ask_to_replay()
+
         
 
 if __name__ == "__main__":
